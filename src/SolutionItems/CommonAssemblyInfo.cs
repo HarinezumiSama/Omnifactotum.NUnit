3312e8165a12e3ba162500ca4cec43417a55870a
Vitalii Maklai <vitalii.maklai@gmail.com>
2015-08-05 04:39:11 -0400

    First version of the 'Omnifactotum.NUnit' project moved from the 'Omnifactotum' source control repository

---
+++ src/SolutionItems/CommonAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Omnifactotum.NUnit: the .NET library intended to help developers avoid writing the same helper types, methods and extension methods for different testing projects using NUnit framework.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vitalii Maklai")]
+[assembly: AssemblyProduct("Omnifactotum.NUnit")]
+[assembly: AssemblyCopyright("Copyright © Vitalii Maklai 2013-2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
