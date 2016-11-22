972b1a5d7bad49e606e58155c5a14d0bee562d4c
Vitalii Maklai <vitalii.maklai@gmail.com>
2016-11-21 21:30:57 -0500

    Introduced the MappingAccordances<TSource, TDestination> class

---
+++ src/Omnifactotum.NUnit/ConstraintCreator.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework.Constraints;
+
+namespace Omnifactotum.NUnit
+{
+    /// <summary>
+    ///     Represents a reference to a method that creates an instance of
+    ///     <see cref="IResolveConstraint"/> using the specified expected value.
+    /// </summary>
+    /// <param name="expectedValue">
+    ///     The expected value.
+    /// </param>
+    /// <typeparam name="TValue">
+    ///     The type of the expected value.
+    /// </typeparam>
+    public delegate IResolveConstraint ConstraintCreator<in TValue>(TValue expectedValue);
+}
---
+++ src/Omnifactotum.NUnit/MappingAccordances.Helper.cs
@@ -0,0 +1,60 @@
+﻿using Omnifactotum.Annotations;
+
+namespace Omnifactotum.NUnit
+{
+    /// <summary>
+    ///     Provides helper functionality for the
+    ///     <see cref="MappingAccordances{TSource,TDestination}"/> class.
+    /// </summary>
+    public static class MappingAccordances
+    {
+        /// <summary>
+        ///     Provides the fluid syntax of creating instances of the
+        ///     <see cref="MappingAccordances{TSource,TDestination}"/> class.
+        /// </summary>
+        /// <typeparam name="TSource">
+        ///     The type of a source object.
+        /// </typeparam>
+        public static class From<TSource>
+        {
+            /// <summary>
+            ///     Creates a new instance of the
+            ///     <see cref="MappingAccordances{TSource,TDestination}"/> class.
+            /// </summary>
+            /// <typeparam name="TDestination">
+            ///     The type of a destination object.
+            /// </typeparam>
+            /// <returns>
+            ///     A new instance of the <see cref="MappingAccordances{TSource,TDestination}"/> class.
+            /// </returns>
+            [Pure]
+            public static MappingAccordances<TSource, TDestination> To<TDestination>()
+                => new MappingAccordances<TSource, TDestination>();
+        }
+
+        /// <summary>
+        ///     Represents a reference to a method that creates an assertion failure message based on
+        ///     the specified source and destination expressions and values.
+        /// </summary>
+        /// <param name="sourceExpression">
+        ///     A string representation of the expression selecting the property in a source object.
+        /// </param>
+        /// <param name="sourceValue">
+        ///     The value of the property in a source object.
+        /// </param>
+        /// <param name="destinationExpression">
+        ///     A string representation of the expression selecting the property in a destination object.
+        /// </param>
+        /// <param name="destinationValue">
+        ///     The value of the property in a destination object.
+        /// </param>
+        /// <typeparam name="TValue">
+        ///     The type of the source and destination properties.
+        /// </typeparam>
+        public delegate string AssertionFailedMessageCreator<in TValue>(
+            string sourceExpression,
+            TValue sourceValue,
+            string destinationExpression,
+            TValue destinationValue);
+    }
+}
