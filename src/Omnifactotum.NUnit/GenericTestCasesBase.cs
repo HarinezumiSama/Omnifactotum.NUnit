94cfd7171edadb4d4f5194ae254edc88604f3d2e
Vitalii Maklai <vitalii.maklai@gmail.com>
2016-12-05 00:35:34 -0500

    Introduced the GenericTestCasesBase<T> class and made it the ancestor of TestCasesBase

---
+++ src/Omnifactotum.NUnit/GenericTestCasesBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Omnifactotum.NUnit
+{
+    /// <summary>
+    ///     Represents the base class providing test cases for a test.
+    /// </summary>
+    /// <typeparam name="T">
+    ///     The type of the object specifying arguments for a test method.
+    /// </typeparam>
+    public abstract class GenericTestCasesBase<T> : IEnumerable<T>
+    {
+        /// <summary>
+        ///     Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        ///     A <see cref="IEnumerator{T}"/> that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator<T> GetEnumerator() => GetCases().AssertNotNull().GetEnumerator();
+
+        /// <summary>
+        ///     Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        ///     An <see cref="IEnumerator"/> object that can be used to iterate through the collection.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        /// <summary>
+        ///     Gets the collection of test cases.
+        /// </summary>
+        /// <returns>
+        ///     The collection of test cases.
+        /// </returns>
+        protected abstract IEnumerable<T> GetCases();
+    }
+}
--- src/Omnifactotum.NUnit/TestCasesBase.cs
+++ src/Omnifactotum.NUnit/TestCasesBase.cs
@@ -1,37 +1,12 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Omnifactotum.NUnit
 {
     /// <summary>
-    ///     Represents the base class providing test cases for a test.
+    ///     Represents the base class providing test cases for a test using <see cref="TestCaseData"/>.
     /// </summary>
-    public abstract class TestCasesBase : IEnumerable<TestCaseData>
+    public abstract class TestCasesBase : GenericTestCasesBase<TestCaseData>
     {
-        /// <summary>
-        ///     Returns an enumerator that iterates through the collection.
-        /// </summary>
-        /// <returns>
-        ///     A <see cref="IEnumerator{TestCaseData}"/> that can be used to iterate through the collection.
-        /// </returns>
-        public IEnumerator<TestCaseData> GetEnumerator() => GetCases().AssertNotNull().GetEnumerator();
-
-        /// <summary>
-        ///     Returns an enumerator that iterates through a collection.
-        /// </summary>
-        /// <returns>
-        ///     An <see cref="IEnumerator"/> object that can be used to iterate through the collection.
-        /// </returns>
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        /// <summary>
-        ///     Gets the collection of test cases.
-        /// </summary>
-        /// <returns>
-        ///     The collection of test cases.
-        /// </returns>
-        protected abstract IEnumerable<TestCaseData> GetCases();
+        // No own members
     }
 }
